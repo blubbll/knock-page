4cf99e6ffa5964cd947bb5bbb3c8a3c43f526952
Glitch (knock-page) <none>
2020-05-24 16:06:31 +0000

    structure

--- public/script.js
+++ public/script.js
@@ -1,51 +1,74 @@
 console.clear();
 
+//selectors
+const $ = document.querySelector.bind(document),
+  $$ = document.querySelectorAll.bind(document);
+
 //get
 const { ko, page } = window;
 //set
 let { model } = window;
 
+//apparently page.js doesn't work without this lol
 page.configure({ window: window });
 
+//setup knock model
 ko.applyBindings(
   new (function() {
     // proxyfying
     var self = this;
     //Data
-    self.folders = ["Inbox", "Archive", "Sent", "Spam"];
-    self.chosenFolderId = ko.observable();
-    self.chosenFolderData = ko.observable();
-    self.chosenMailData = ko.observable();
+    {
+      self.folders = ["Inbox", "Archive", "Sent", "Spam"];
+      self.chosenFolderId = ko.observable();
+      self.chosenFolderData = ko.observable();
+      self.chosenMailData = ko.observable();
+    }
 
     // Behaviours
-    self.goToFolder = folder => {
-      //page(`/folder/${folder}`)
+    {
+      self.goToIndex = () => {
+        self.chosenFolderData(null);
+        self.chosenMailData(null);
+      };
 
-      self.chosenFolderId(folder);
-      //stop showing mail
-      self.chosenMailData(null);
-      //get data
-      fetch(`/mail?folder=${folder || null}`)
-        .then(r => r.json())
-        .then(self.chosenFolderData);
-    };
+      self.goToFolder = folder => {
+        page(`/folder/${folder}`);
 
-    self.goToMail = mail => {
-      self.chosenFolderId(mail.folder);
-      //stop showing folder
-      self.chosenFolderData(null);
-      //get data
-      fetch(`/mail?mailId=${mail.id || null}`)
-        .then(r => r.json())
-        .then(self.chosenMailData);
-    };
+        self.chosenFolderId(folder);
+        //stop showing mail
+        self.chosenMailData(null);
+        //get data
+        fetch(`/mail?folder=${folder || null}`)
+          .then(r => r.json())
+          .then(self.chosenFolderData);
+      };
+
+      self.goToMail = mail => {
+        self.chosenFolderId(mail.folder);
+        //stop showing folder
+        self.chosenFolderData(null);
+        //get data
+        fetch(`/mail?mailId=${mail.id || null}`)
+          .then(r => r.json())
+          .then(self.chosenMailData);
+      };
+    }
 
     //expose model func
     model = this;
   })()
 );
+
 //go to default path
 model.goToFolder("Inbox");
+
+//setup Routes
+{
+  page("/", model.goToIndex);
+  page("/folder/:folder", () => {});
+}
+
 /*page('/', index)
 page('/user/:user', show)
 page('/user/:user/edit', edit)
--- server.js
+++ server.js
@@ -3,21 +3,22 @@
 const //imports
   express = require("express"),
   app = express(),
-  fetch = require("node-fetch");
+  fetch = require("node-fetch"),
+      fs = require("fs");
 
 //static
 app.use(express.static("public"));
 
-//start
-app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/views/index.html`);
-});
 
-// forward ajaxes
+// routing / ajax forwarding
 app.get("/*", (req, res) => {
+  if(req.originalUrl.includes("?"))
   fetch(`http://learn.knockoutjs.com${req.originalUrl}`)
     .then(r => r.json())
     .then(j => res.send(j));
+  else{
+    res.send(fs.readFileSync(`${__dirname}/views/index.html`, "utf8").replace("{{from}}", req.originalUrl));
+  }
 });
 
 // listen
